de29bcf8dfa5097592436679619eba3d6fbb026b
uknomzaying <121236013+Anzuniks@users.noreply.github.com>
2026-04-14 13:59:51 +0300

    Päivitän

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './views/Login';
 import Logout from './views/Logout';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/register" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/logout" element={<Logout />} />
-
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+        </Routes>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { HashRouter } from 'react-router-dom' // Vaihdettu tästä
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* HashRouterin kanssa et tarvitse basenamea! */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
